9adde7396dc0a9c529974a6ec000bab1e48ceca1
justin <justinzhou@canway.net>
2026-04-08 16:23:37 +0800

    feature:目录接口返回添加account_expiration_notice_interval(用户勾选的到期提醒天数)

--- src/api/bkuser_core/categories/serializers.py
+++ src/api/bkuser_core/categories/serializers.py
@@ -8,6 +8,7 @@ Unless required by applicable law or agreed to in writing, software distributed 
 an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
 specific language governing permissions and limitations under the License.
 """
+from typing import List, Optional
 
 from django.utils.translation import ugettext_lazy as _
 from rest_framework import serializers
@@ -16,6 +17,7 @@ from rest_framework.validators import ValidationError
 from bkuser_core.apis.v2.serializers import CustomFieldsModelSerializer
 from bkuser_core.categories.models import ProfileCategory
 from bkuser_core.profiles.validators import validate_domain
+from bkuser_core.user_settings.constants import ACCOUNT_EXPIRATION_NOTICE_INTERVAL_META_KEY
 
 
 class CategorySerializer(CustomFieldsModelSerializer):
@@ -32,6 +34,20 @@ class CategorySerializer(CustomFieldsModelSerializer):
     # def get_unfilled_namespaces(self, obj) -> List[str]:
     #     unfilled_nss = set(obj.get_unfilled_settings().values_list("namespace", flat=True))
     #     return list(unfilled_nss)
+
+    account_expiration_notice_interval = serializers.SerializerMethodField(
+        help_text="账号到期提醒时间间隔（天）"
+    )
+
+    def get_account_expiration_notice_interval(self, obj: ProfileCategory) -> Optional[List[int]]:
+        """获取账号到期提醒时间间隔配置"""
+        setting = obj.settings.filter(
+            meta__key=ACCOUNT_EXPIRATION_NOTICE_INTERVAL_META_KEY,
+            enabled=True
+        ).first()
+        if setting:
+            return setting.value
+        return None
 
     class Meta:
         model = ProfileCategory
